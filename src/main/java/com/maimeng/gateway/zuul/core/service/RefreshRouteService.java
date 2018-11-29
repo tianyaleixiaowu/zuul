e3d4db8d0cce6fc3d2b02adb8e8a8dc0aa118444
wuwf <272551766@qq.com>
2018-11-29 15:33:50 +0800

    1

--- src/main/java/com/maimeng/gateway/zuul/core/service/RefreshRouteService.java
+++ src/main/java/com/maimeng/gateway/zuul/core/service/RefreshRouteService.java
@@ -3,6 +3,9 @@ package com.maimeng.gateway.zuul.core.service;
 import org.springframework.cloud.netflix.zuul.RoutesRefreshedEvent;
 import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -11,6 +14,8 @@ import javax.annotation.Resource;
  * Created by wuweifeng on 2017/10/12.
  */
 @Service
+@EnableScheduling
+@EnableAsync
 public class RefreshRouteService {
     @Resource
     private ApplicationEventPublisher publisher;
@@ -21,5 +26,11 @@ public class RefreshRouteService {
     public void refreshRoute() {
         RoutesRefreshedEvent routesRefreshedEvent = new RoutesRefreshedEvent(routeLocator);
         publisher.publishEvent(routesRefreshedEvent);
+    }
+
+
+    @Scheduled(cron = "0 0/2 * * * ?")
+    public void fetch() {
+        refreshRoute();
     }
 }
